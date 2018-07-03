73267bac53178222be7fc263a67ade6fc2dc04f4
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-07-03 17:21:13 +0300

    Add project files.

---
+++ HTTP_LINQ_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTTP_LINQ_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
