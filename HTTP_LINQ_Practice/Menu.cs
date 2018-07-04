63cceddb3bdecb26f9b554c9bf0b624df92f0757
bogdaner2 <KonorinDeveloper@gmail.com>
2018-07-04 21:01:09 +0300

    Update Menu.cs

--- HTTP_LINQ_Practice/Menu.cs
+++ HTTP_LINQ_Practice/Menu.cs
@@ -72,7 +72,7 @@ namespace HTTP_LINQ_Practice
             var posts = DownloadDataAsync<List<Post>>("https://5b128555d50a5c0014ef1204.mockapi.io/posts").Result;
             var todos = DownloadDataAsync<List<ToDo>>("https://5b128555d50a5c0014ef1204.mockapi.io/todos").Result;
             var comments = DownloadDataAsync<List<Comment>>("https://5b128555d50a5c0014ef1204.mockapi.io/comments").Result;
-            posts.ForEach(p => p.Comments = comments.Where(c => p.Id == c.Id).ToList());
+            posts.ForEach(p => p.Comments = comments.Where(c => p.Id == c.PostId).ToList());
             users.ForEach(user => user.Posts = posts.Where(x => user.Id == x.UserId).ToList());
             users.ForEach(user => user.ToDos = todos.Where(x => user.Id == x.UserId).ToList());
             Request.Users = users;
